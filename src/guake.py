3e87d2c8105b2509b7f809139180e027899b3316
Lincoln de Sousa <lincoln@alfaiati.net>
2009-02-11 16:18:11 -0200

    Updating copyright notice.

--- src/guake.py
+++ src/guake.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; -*-
 """
+Copyright (C) 2007,2008,2009 Lincoln de Sousa <lincoln@minaslivre.org>
 Copyright (C) 2007 Gabriel Falcão <gabrielteratos@gmail.com>
-Copyright (C) 2007,2008 Lincoln de Sousa <lincoln@minaslivre.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
