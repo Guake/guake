2c549f13262c884e97d419778eeacbb93410c37c
Lincoln de Sousa <lincoln@alfaiati.net>
2009-04-14 16:22:24 -0300

    Updating copyright info in some files.

--- src/common.py
+++ src/common.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; -*-
 """
+Copyright (C) 2007-2009 Lincoln de Sousa <lincoln@minaslivre.org>
 Copyright (C) 2007 Gabriel Falcão <gabrielteratos@gmail.com>
-Copyright (C) 2007 Lincoln de Sousa <lincoln@archlinux-br.org>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License as
