14788701879f72ceaeaa3494d3f9b56fa5bc2b68
Gaetan Semet <gaetan@xeberon.net>
2014-07-11 16:12:06 +0200

    Remove redundancy in pref (#344)
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dev.sh
+++ dev.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ ! -f configure ] && ./autogen.sh
+
 make && sudo make install && gconftool-2 --install-schema-file=/usr/local/etc/gconf/schemas/guake.schemas || exit 1
 
 guake --quit 2> /dev/null
