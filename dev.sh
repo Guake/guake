01ca618c08c9a5243386ac53d7b6184b5cc999ec
Gaetan Semet <gaetan@xeberon.net>
2014-11-23 14:43:03 +0100

    dev --no-install
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dev.sh
+++ dev.sh
@@ -3,6 +3,16 @@
 # This script is used by the main developer to quickly compile and install the current version
 # of Guake sources. Nothing say it will work directly on your environment. Use with caution!
 
+NO_INSTALL=false
+
+echo "execute guake for developer."
+echo "use --no-install to avoid installing guake on your system"
+echo "(beware, gconf schema will be altered)"
+
+if [[ $1 == "--no-install" ]]; then
+    NO_INSTALL=true
+fi
+
 if [[ ! -f configure ]]; then
     sudo apt-get install -y build-essential python autoconf
     sudo apt-get install -y gnome-common gtk-doc-tools libglib2.0-dev libgtk2.0-dev libgconf2-dev
@@ -12,7 +22,14 @@ if [[ ! -f configure ]]; then
     ./autogen.sh
 fi
 
-make && sudo make install && gconftool-2 --install-schema-file=/usr/local/etc/gconf/schemas/guake.schemas || exit 1
+make || exit 1
 
-guake --quit 2> /dev/null
-guake
+if [[ $NO_INSTALL == true ]]; then
+    gconftool-2 --install-schema-file=data/guake.schemas
+    PYTHONPATH=src python src/guake/main.py
+else
+  sudo make install && gconftool-2 --install-schema-file=/usr/local/etc/gconf/schemas/guake.schemas || exit 1
+
+  guake --quit 2> /dev/null
+  guake
+fi
