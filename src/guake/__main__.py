17829b443a7176fd7d2d0fa6c3eec2243e22c358
Gaetan Semet <gaetan@xeberon.net>
2014-11-23 14:43:03 +0100

    dev --no-install
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- src/guake/__main__.py
+++ src/guake/__main__.py
@@ -1,17 +1,5 @@
 from __future__ import absolute_import
 
-import gtk
+from guake.main import exec_main
 
-from guake.common import ShowableError
-from guake.common import _
-from guake.common import test_gconf
-from guake.main import main
-
-if __name__ == '__main__':
-    if not test_gconf():
-        raise ShowableError(_('Guake can not init!'),
-                            _('Gconf Error.\n'
-                              'Have you installed <b>guake.schemas</b> properly?'))
-
-    if not main():
-        gtk.main()
+exec_main()
