b52953b1014f2facb83beba9eb2055bb29fc0672
Gaetan Semet <gaetan@xeberon.net>
2015-08-21 16:41:00 +0200

    Start working on Guake GTK3
    Test VTE: ok
    Test dconf: ?
    Test pango: ?
    
    Need brand new installation scripts, no more all the
    mess with autoconf since we don't compile C anymore
    
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- src-gtk3/guake/notifier.py
+++ src-gtk3/guake/notifier.py
@@ -20,6 +20,7 @@ Boston, MA 02111-1307, USA.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import unicode_literals
 
 from testwrap import dedent
 
