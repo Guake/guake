1ba7f977fefc4f139f992ee7bc4315c72a32330f
Gaetan Semet <gaetan@xeberon.net>
2015-10-21 12:54:16 +0200

    Fresh clean for Guake 3
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- dev-gtk3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "execute Guake GTK3 for developer."
-
-sudo ./install.py
-
-PYTHONPATH=src-gtk3 python3 src-gtk3/guake/gtk3test.py
--- src/guake/__main__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
-from guake.main import exec_main
-
-
-exec_main()
