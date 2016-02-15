5e1b7a5fba53adf2f491b61caecf61a9a9294e58
Gaetan Semet <gaetan@xeberon.net>
2016-02-15 22:11:17 +0100

    setup logging and base for gtk vte integration
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ guake/test_terminal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import gi
+
+from gi.repository import GLib
+from gi.repository import Gtk
+from gi.repository import Vte
+
+
+def test():
+    # gi.require_version('Gtk', '3.0')
+
+    terminal = Vte.Terminal()
+    terminal.fork_command_full(
+        Vte.PtyFlags.DEFAULT,
+        os.environ['HOME'],
+        ["/bin/bash"],
+        [],
+        GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+        None,
+        None,
+    )
+
+    win = Gtk.Window()
+    win.connect('delete-event', Gtk.main_quit)
+    win.add(terminal)
+    win.show_all()
+
+    Gtk.main()
