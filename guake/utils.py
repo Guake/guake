53b90b1e9e41ef78772016ff91efbe6319382939
Mikhail Koshel <mouseratti@gmail.com>
2016-12-23 02:23:17 +0600

    #879 added attach_methods function

---
+++ guake/utils.py
@@ -0,0 +1,13 @@
+from types import MethodType
+
+def attach_methods(Src, dest):
+    """Attach methods of class Src to an instance dest"""
+    for attr_name in dir(Src):
+        if not attr_name.startswith("__"):
+            attr = Src.__dict__[attr_name]
+            if callable(attr):
+                try:
+                    dest.__getattribute__(attr_name)
+                except AttributeError:
+                    dest.__setattr__(attr_name, MethodType(attr, dest))
+    return
