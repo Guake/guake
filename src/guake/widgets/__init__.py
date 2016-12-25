1cd5d302e90203cb0a50735aa1fa18edeb3289ee
Mikhail Koshel <mouseratti@gmail.com>
2016-12-26 05:58:16 +0600

    updated GuakeApplicationWindow; logic of __new__ method moved to GuakeWidget #879

---
+++ src/guake/widgets/__init__.py
@@ -0,0 +1,1 @@
+from guake.widgets.widget import GuakeWidget
--- src/guake/widgets/application_window.py
+++ src/guake/widgets/application_window.py
@@ -1,42 +1,42 @@
-import os
 import logging
 from guake import gi
 from gi.repository import Gtk
 from gi.repository import Gdk
-
+from guake.widgets import GuakeWidget
 logger = logging.getLogger(__name__)
 
-class GuakeApplicationWindow(Gtk.ApplicationWindow):
+class GuakeApplicationWindow(GuakeWidget, Gtk.ApplicationWindow):
 
     __filename__ = "app.ui"
-
-    def __new__(cls, *args, **kwargs):
-        """Create application from glade .ui file; ApplicationWindow identifier 
-        in the ui-file should be equal to cls.__name__"""
-        _datapath = kwargs.get("datapath", "./data")
-        filename = os.path.join(_datapath, "ui", cls.__filename__)
-        builder = Gtk.Builder()
-        builder.add_from_file(filename)
-        instance = builder.get_object(cls.__name__)
-        assert instance is not None, "Gtk widget %s not found!" % cls.__name__
-        instance.__class__ = cls
-        del(builder)
-        return instance
 
     def __init__(self, *args, **kwargs):
         app = kwargs.get("application")
         if app is not None:
             self.set_application(app)
-        self._visible = kwargs.get("visible", False)
         self._set_window_position()
         self._set_window_size()
+        self.visible = kwargs.get("visible", False)
 
-    def _get_screen(self):
+    @property
+    def visible(self):
+        return self._visible
+    
+    @visible.setter
+    def visible(self, value):
+        self._visible = value
+        if value:
+            self.show_all()
+            self.present()
+            return
+        self.hide()
+        return
+
+    def _select_screen(self):
         # TODO: get tagret screen from settings
         return Gdk.Screen.get_default()
 
     def _get_screen_size(self):
-        screen = self._get_screen()
+        screen = self._select_screen()
         return (screen.get_width(), screen.get_height())
 
     def _set_window_size(self):
@@ -47,7 +47,7 @@ class GuakeApplicationWindow(Gtk.ApplicationWindow):
 
         """
         # TODO: read height_setting from settings
-        height_rate = 0.6
+        height_rate = 0.7
         screen_width, screen_height = self._get_screen_size()
         self.set_default_size(screen_width, screen_height * height_rate)
 
@@ -57,12 +57,5 @@ class GuakeApplicationWindow(Gtk.ApplicationWindow):
 
     # handlers
     def show_hide_handler(self, *args):
-        logger.info(args)
-        if self._visible:
-            self.hide()
-            self._visible = False
-            return
-        self.show_all()
-        self.present()
-        self._visible = True
+        self.visible = not self.visible
         return
