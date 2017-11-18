117509569b52c380d2bfce81ce04b37fc2885171
Gaetan Semet <gaetan@xeberon.net>
2017-11-18 18:05:46 +0100

    pipenv --venv

--- run-local.sh
+++ run-local.sh
@@ -2,11 +2,10 @@
 
 echo "execute Guake GTK3 for developer."
 
-# Hack to find the virtualenv path
-VIRTUALENV_PATH=$(export | grep ' PATH=' | cut -d'=' -f2 | cut -d':' -f1 | sed 's/\/bin$//' | sed 's/^"//')
+VIRTUALENV_PATH=$(pipenv --venv)
 
 source $VIRTUALENV_PATH/bin/activate
-# Hack to enable global site packages (pipenv does not provide this feature)
+
 bash <<EOF
 python 2>/dev/null <<EOC
 import gi
