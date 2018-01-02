b02edbca06e554269a8da34c3ee4d35ef36c61ee
Gaetan Semet <gaetan@xeberon.net>
2018-01-03 00:53:49 +0100

    update pipenv

--- bootstrap-pip.sh
+++ bootstrap-pip.sh
@@ -4,7 +4,7 @@ echo "Freeze version of pip to ensure reproductibility"
 
 pip install --user --upgrade \
     'pip==9.0.1' \
-    'pipenv==8.3.2' \
+    'pipenv==9.0.1' \
     'setuptools==36.6.0' \
     || echo "you may need to sudo me !"
 
