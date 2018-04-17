f440b4767831c3b1f48d106b520f462f29397a53
Gaetan Semet <gaetan@xeberon.net>
2018-04-17 10:32:17 +0200

    Fix bad for vte hyperlink call
    ref #1264
    
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- guake/terminal.py
+++ guake/terminal.py
@@ -286,7 +286,7 @@ class GuakeTerminal(Vte.Terminal):
 
         if event.button == 1 and (event.get_state() & Gdk.ModifierType.CONTROL_MASK):
             if (Vte.MAJOR_VERSION, Vte.MINOR_VERSION) > (0, 50):
-                s = self.hyperlink_check_event()
+                s = self.hyperlink_check_event(event)
                 self._on_ctrl_click_matcher((s, None))
             elif self.get_has_selection():
                 self.quick_open()
