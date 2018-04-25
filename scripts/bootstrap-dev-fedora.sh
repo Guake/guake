4ebb92ec5aa1988b6d9f2821b828f7c6c7be9b0b
Mario Aichinger <aichingm@gmail.com>
2018-04-25 14:29:09 +0200

    fix typo in fedora package name

--- scripts/bootstrap-dev-fedora.sh
+++ scripts/bootstrap-dev-fedora.sh
@@ -4,7 +4,7 @@ echo "Install packages needed for execution"
 
 sudo apt install -y \
     python3-devel \
-    python3-cario \
+    python3-cairo \
     python3-dbus \
     python3-pip \
     keybinder3 \
