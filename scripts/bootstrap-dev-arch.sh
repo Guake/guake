2634ad1a2bf9c543b372b24da4fed2f0ab4e0502
Mario Aichinger <aichingm@gmail.com>
2018-04-27 21:51:48 +0200

    add option groups (run, make, dev, opt) to the bootstrap scripts

--- scripts/bootstrap-dev-arch.sh
+++ scripts/bootstrap-dev-arch.sh
@@ -1,12 +1,71 @@
 #!/bin/bash
 
-echo "Install packages needed for execution"
-sudo pacman -S \
-    libkeybinder3 \
-    libnotify \
-    libutempter \
-    python-cairo \
-    python-dbus \
-    python-gobject \
-    python-pbr \
-    vte3 \
+RUN=0
+MAKE=0
+DEV=0
+OPT=0
+ARGC="$#"
+
+while test $# -gt 0
+do
+    case "$1" in
+        run)
+          RUN=1
+          ;;
+        make)
+          MAKE=1
+          ;;
+        dev)
+          DEV=1
+          ;;
+        opt)
+          OPT=1
+          ;;
+    esac
+    shift
+done
+
+if [[ "$ARGC" == "0" ]]; then
+          RUN=1;
+          MAKE=1;
+          DEV=1;
+          OPT=0;
+fi
+
+
+if [[ $RUN == "1" ]]; then
+    echo "Install packages needed for execution"
+    sudo pacman -S \
+        libkeybinder3 \
+        libnotify \
+        python-cairo \
+        python-dbus \
+        python-gobject \
+        python-pbr \
+        vte3
+fi
+
+if [[ $MAKE == "1" ]]; then
+    echo "Install packages needed for making guake"
+    sudo pacman -S \
+        gettext \
+        gsettings-desktop-schemas \
+        make \
+        pandoc
+fi
+
+if [[ $DEV == "1" ]]; then
+    echo "Install needed development packages"
+    sudo pacman -S \
+        dconf-editor \
+        glade \
+        poedit \
+        gnome-tweak-tool
+fi
+
+if [[ $OPT == "1" ]]; then
+echo "Install packages optional for execution"
+    sudo pacman -S \
+        libutempter \
+        numix-gtk-theme
+fi
--- scripts/bootstrap-dev-debian.sh
+++ scripts/bootstrap-dev-debian.sh
@@ -1,36 +1,76 @@
 #!/bin/bash
 
-echo "Install packages needed for execution"
+RUN=0
+MAKE=0
+DEV=0
+OPT=0
+ARGC="$#"
 
-sudo apt install -y \
-    gir1.2-keybinder-3.0 \
-    gir1.2-notify-0.7 \
-    gir1.2-vte-2.91 \
-    libkeybinder-3.0-0 \
-    python3 \
-    python3-cairo \
-    python3-dbus \
-    python3-gi \
-    python3-pbr \
-    python3-pip \
+while test $# -gt 0
+do
+    case "$1" in
+        run)
+          RUN=1
+          ;;
+        make)
+          MAKE=1
+          ;;
+        dev)
+          DEV=1
+          ;;
+        opt)
+          OPT=1
+          ;;
+    esac
+    shift
+done
 
-echo "Install needed development packages on a Debian/Ubuntu system"
-sudo apt install -y \
-    aspell-fr \
-    colortest \
-    dconf-editor \
-    gettext \
-    glade \
-    poedit \
-    gnome-tweak-tool \
-    gsettings-desktop-schemas \
-    make \
-    pandoc \
+if [[ "$ARGC" == "0" ]]; then
+          RUN=1;
+          MAKE=1;
+          DEV=1;
+          OPT=0;
+fi
 
-if [[ $1 == "--with-optional" ]]; then
 
+if [[ $RUN == "1" ]]; then
+    echo "Install packages needed for execution"
+    sudo apt install -y \
+        gir1.2-keybinder-3.0 \
+        gir1.2-notify-0.7 \
+        gir1.2-vte-2.91 \
+        libkeybinder-3.0-0 \
+        python3 \
+        python3-cairo \
+        python3-dbus \
+        python3-gi \
+        python3-pbr \
+        python3-pip
+fi
+
+if [[ $MAKE == "1" ]]; then
+    echo "Install packages needed for making guake"
+    sudo apt install -y \
+        gettext \
+        gsettings-desktop-schemas \
+        make \
+        pandoc
+fi
+
+if [[ $DEV == "1" ]]; then
+    echo "Install needed development packages"
+    sudo apt install -y \
+        aspell-fr \
+        colortest \
+        dconf-editor \
+        glade \
+        poedit \
+        gnome-tweak-tool
+fi
+
+if [[ $OPT == "1" ]]; then
+    echo "Install packages optional for execution"
     sudo apt install -y \
         libutempter0 \
         numix-gtk-theme \
-
 fi
