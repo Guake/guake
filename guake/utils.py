ad7c87d2e871b541b35d2bf1ddd891219841262c
Mario Aichinger <aichingm@gmail.com>
2018-09-03 13:55:18 +0200

    add comments

--- guake/utils.py
+++ guake/utils.py
@@ -68,15 +68,25 @@ class TabNameUtils():
 class HidePrevention():
 
     def __init__(self, window):
+        """Create a new HidePrevention object like `HidePrevention(window)`
+        """
         self.window = window
 
     def may_hide(self):
+        """returns True if the window is allowed to hide and
+        False if `prevent()` is called from some where
+        """
         return getattr(self.window, 'can_hide', True)
 
     def prevent(self):
+        """sets a flag on the window object which indicates to
+        may_hide that the window is NOT allowed to be hidden.
+        """
         setattr(self.window, 'can_hide', False)
 
     def allow(self):
+        """sets the flag so that it indicates to may_hide that the window is allowed to be hidden
+        """
         setattr(self.window, 'can_hide', True)
 
 
