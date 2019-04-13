5bb99d45b75e7ca8faeeccdac056500bde070d73
Louie Lu <git@louie.lu>
2019-04-13 20:14:12 +0800

    Address gsemet pytest points

--- guake/tests/test_about.py
+++ guake/tests/test_about.py
@@ -7,21 +7,16 @@ from guake.about import AboutDialog
 from locale import gettext as _
 
 
-class TestAboutDialog:
+@pytest.fixture
+def dialog(mocker):
+    mocker.patch('guake.simplegladeapp.Gtk.Widget.show_all')
+    ad = AboutDialog()
+    return ad
 
-    @classmethod
-    def setup_class(cls):
-        cls.ad = AboutDialog()
-        cls.dialog = cls.ad.get_widget('aboutdialog')
 
-    @classmethod
-    def teardown_class(cls):
-        cls.dialog.destroy()
+def test_version_test(dialog):
+    assert dialog.get_widget('aboutdialog').get_version() == guake_version()
 
-    @pytest.mark.dialog
-    def test_version_text(self):
-        assert self.dialog.get_version() == guake_version()
 
-    @pytest.mark.dialog
-    def test_title(self):
-        assert self.dialog.get_title() == _('About Guake')
+def test_title(dialog):
+    assert dialog.get_widget('aboutdialog').get_title() == _('About Guake')
