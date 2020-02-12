aa3dc436a0707ce3008e3f370f28b92b849f4bcd
ruro <ruro.ruro@ya.ru>
2020-02-12 13:31:10 +0300

    fix: double click in tmux/vim

--- guake/notebook.py
+++ guake/notebook.py
@@ -115,9 +115,6 @@ class TerminalNotebook(Gtk.Notebook):
                 # Gtk 3.18 fallback ("'Menu' object has no attribute 'popup_at_pointer'")
                 menu.popup(None, None, None, None, event.button, event.time)
 
-        elif event.type == Gdk.EventType.DOUBLE_BUTTON_PRESS and event.button == 1:
-            self.new_page_with_focus()
-
         return False
 
     @save_tabs_when_changed
