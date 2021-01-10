962b407d47dc57fbdaa5e6f2cac8f412119dea5b
zeusintuivo <zeus@intuivo.com>
2021-01-10 16:13:02 +0100

    added build script to make building and rebuilding easier

---
+++ build_guake.bash
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+function main(){
+pkill guake                                                        # incase is running in the background
+source env/bin/activate     # Go into your virtualenv environment
+make
+sudo make dev               # build with sudo to warranty a clean build
+source deactivate           # Get out of your virtualenv environment
+sudo make install           # install with sudo to replace your current executables
+guake --show --verbose      # or in case you also want to see what is going on.
+}
+
+time main
