eb6f84754907873b84bf9f57c961e12d1b1d5ef1
zeusintuivo <zeus@intuivo.com>
2021-01-10 16:04:53 +0100

    removed Fix ineffective override of GDK_BACKEND envvar

--- guake/main.py
+++ guake/main.py
@@ -36,8 +36,8 @@ from optparse import OptionParser
 
 log = logging.getLogger(__name__)
 
-# Force use X11 backend under wayland before any import of GDK through dependencies
-os.environ["GDK_BACKEND"] = "x11"
+# Force use X11 backend under wayland before any import of GDK through dependencies - This makes it floating and hides the icon but fixes other settings in the GUI
+# os.environ["GDK_BACKEND"] = "x11"
 
 from guake.globals import NAME
 from guake.globals import bindtextdomain
@@ -65,6 +65,9 @@ def main():
     """
     # Force to xterm-256 colors for compatibility with some old command line programs
     os.environ["TERM"] = "xterm-256color"
+
+    # Force use X11 backend underwayland
+    os.environ["GDK_BACKEND"] = "x11"
 
     # do not use version keywords here, pbr might be slow to find the version of Guake module
     parser = OptionParser()
