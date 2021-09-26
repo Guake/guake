67470a437010dc4e68d6fec3fda44424ca706d03
zeusintuivo <zeus@intuivo.com>
2021-09-26 16:05:38 +0200

    merged support.py from master

--- guake/support.py
+++ guake/support.py
@@ -4,12 +4,9 @@ import os
 
 import gi
 
-gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
 
 from gi.repository import Gdk
-from gi.repository import GdkX11
-from gi.repository import Gtk
 
 from guake import gtk_version
 from guake import guake_version
@@ -23,75 +20,63 @@ def horizonal_line():
 
 def populate_display(display):
     screen = display.get_default_screen()
-    print("Display: {}".format(display.get_name()))
+    print(f"Display: {display.get_name()}")
     print()
     # pylint: disable=R1719
-    print("RGBA visual: {}".format(True if screen.get_rgba_visual() else False))
+    print(f"RGBA visual: {True if screen.get_rgba_visual() else False}")
     print()
-    print("Composited: {}".format(screen.is_composited()))
+    print(f"Composited: {screen.is_composited()}")
     print()
 
     n_monitors = display.get_n_monitors()
     for i in range(n_monitors):
         monitor = display.get_monitor(i)
-        manufacturer = monitor.get_manufacturer()
-        model = monitor.get_model()
-        v = "%s%s%s" % (
-            manufacturer if manufacturer else "",
-            " " if manufacturer or model else "",
-            model if model else "",
-        )
-        print("* Monitor: {} - {}".format(i, v))
+        v = " ".join(j for j in (monitor.get_manufacturer(), monitor.get_model()) if j)
+        print(f"* Monitor: {i} - {v}")
 
         # Geometry
         rect = monitor.get_geometry()
         scale = monitor.get_scale_factor()
-        v = "%d x %d%s at %d, %d" % (
-            rect.width,
-            rect.height,
-            " % 2" if scale == 2 else "",
-            rect.x,
-            rect.y,
-        )
-        print("    * Geometry:\t\t{}".format(v))
+        v = f"{rect.width} x {rect.height}{' % 2' if scale == 2 else ''} at {rect.x}, {rect.y}"
+        print(f"    * Geometry:\t\t{v}")
 
         # Size
-        v = "%d x %d mm²" % (monitor.get_width_mm(), monitor.get_height_mm())
-        print("    * Size:\t\t{}".format(v))
+        v = f"{monitor.get_width_mm()} x {monitor.get_height_mm()} mm²"
+        print(f"    * Size:\t\t{v}")
 
         # Primary
-        print("    * Primary:\t\t{}".format(monitor.is_primary()))
+        print(f"    * Primary:\t\t{monitor.is_primary()}")
 
         # Refresh rate
         if monitor.get_refresh_rate():
-            v = "%.2f Hz" % (0.001 * monitor.get_refresh_rate())
+            v = f"{0.001 * monitor.get_refresh_rate()} Hz"
         else:
             v = "unknown"
-        print("    * Refresh rate:\t{}".format(v))
+        print(f"    * Refresh rate:\t{v}")
 
         # Subpixel layout
-        print("    * Subpixel layout:\t{}".format(monitor.get_subpixel_layout().value_nick))
+        print(f"    * Subpixel layout:\t{monitor.get_subpixel_layout().value_nick}")
 
 
 def get_version():
     display = Gdk.Display.get_default()
 
-    print("Guake Version:\t\t{}".format(guake_version()))
+    print(f"Guake Version:\t\t{guake_version()}")
     print()
-    print("Vte Version:\t\t{}".format(vte_version()))
+    print(f"Vte Version:\t\t{vte_version()}")
     print()
-    print("Vte Runtime Version:\t{}".format(vte_runtime_version()))
+    print(f"Vte Runtime Version:\t{vte_runtime_version()}")
     print()
     horizonal_line()
-    print("GTK+ Version:\t\t{}".format(gtk_version()))
+    print(f"GTK+ Version:\t\t{gtk_version()}")
     print()
-    print("GDK Backend:\t\t{}".format(str(display).split(" ")[0][1:]))
+    print(f"GDK Backend:\t\t{str(display).split(' ', maxsplit=1)[0]}")
     print()
     horizonal_line()
 
 
 def get_desktop_session():
-    print("Desktop Session: {}".format(os.environ.get("DESKTOP_SESSION")))
+    print(f"Desktop Session: {os.environ.get('DESKTOP_SESSION')}")
     print()
     horizonal_line()
 
