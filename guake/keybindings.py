06adf4ba0898d047d2206581de94c84037c293e9
vantu5z <vantu5z@mail.ru>
2021-10-25 15:47:00 +0300

    remove unused Gdk import

--- guake/keybindings.py
+++ guake/keybindings.py
@@ -23,9 +23,7 @@ import logging
 import gi
 
 gi.require_version("Gtk", "3.0")
-gi.require_version("Gdk", "3.0")
 from gi.repository import Gtk
-from gi.repository import Gdk
 
 from guake import notifier
 from guake.common import pixmapfile
