ac2a15532b2251af53cc233ceab41816180856b3
David Yang <davidyang6us@gmail.com>
2022-01-11 22:08:20 +0800

    Fix upload to pypi test

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import setuptools
-setuptools.setup(use_scm_version={"write_to": "guake/_version.py"})
+
+setuptools.setup(
+    use_scm_version={"write_to": "guake/_version.py", "local_scheme": "no-local-version"}
+)
